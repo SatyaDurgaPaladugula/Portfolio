a7af6d3a8671675d5d7127c6454814aa8c9858dd
Krishna <krishnasritarun95@gmail.com>
2024-11-07 22:06:57 +0530

    fixed the card width

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line react/prop-types
 function Cards({ hs, imgSrc, description }) {
       return (
-            <div className="card d-flex font-medium align-items-center p-3 m-3 text-white">
-                  <img className="h-56 w-96 img-fluid mb-4" src={imgSrc} alt="project visual" />
-                  <h2 className="text-lg mb-2">{hs}</h2>
-                  <p className="text-gray-300 align-items-center">{description}</p>
-            </div>
+            <>
+                  <div className=" card d-flex font-medium align-items-center p-6 rounded-md p-3 m-3 text-white h-96">
+                        <img className="h-56 w-96 img-fluid mb-4 rounded-md" src={imgSrc} alt="project visual" />
+                        <h2 className="text-lg mb-2">{hs}</h2>
+                        <p className="text-gray-300 align-items-center">{description}</p>
+                  </div>
+            </>
       );
 }
 
